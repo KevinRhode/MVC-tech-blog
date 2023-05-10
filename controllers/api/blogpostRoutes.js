58b91b4c5503fb9dad95cb63abcfa3beb4922bb0
Kevin Rhode <kevinrhode@hotmail.com>
2023-05-09 22:51:03 -0400

    blogpost route work

--- controllers/api/blogpostRoutes.js
+++ controllers/api/blogpostRoutes.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const { Blogpost } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+router.post('/', async (req, res) => {
   try {
-    const newBlogpost = await Post.create({
+    const newBlogpost = await Blogpost.create({
       ...req.body,
       user_id: req.session.user_id,
     });
@@ -17,7 +17,7 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const blogpostData = await Post.destroy({
+    const blogpostData = await Blogpost.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
